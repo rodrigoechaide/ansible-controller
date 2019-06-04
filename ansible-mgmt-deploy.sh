3ed6e1b313b578c84c0fca244c0e87b7ceb76c3e
Rodrigo Echaide <rodrigoechaide@gmail.com>
2019-06-04 09:58:23 -0300

    Modified way to install pip3 in ansible-mgmt-deploy.sh

--- ansible-mgmt-deploy.sh
+++ ansible-mgmt-deploy.sh
@@ -2,7 +2,7 @@
 
 apt-get update && apt-get upgrade
 apt-get install -y sshpass \
-			   python3-distutils
-curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
-python3 get-pip.py
+			   	   python3-distutils
+
+curl -s https://bootstrap.pypa.io/get-pip.py | python3
 pip3 install ansible
