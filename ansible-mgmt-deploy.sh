f430c7fa96f62edf2ff6fe7dd34f16ca0d449d70
Rodrigo Echaide <rechaide@ascentio.com.ar>
2019-06-21 15:56:22 -0300

    Update ansible-mgmt-deploy.sh

--- ansible-mgmt-deploy.sh
+++ ansible-mgmt-deploy.sh
@@ -2,7 +2,7 @@
 
 apt-get update && apt-get upgrade
 apt-get install -y sshpass \
-			   	   python3-distutils
+                   python3-distutils
 
 curl -s https://bootstrap.pypa.io/get-pip.py | python3
 pip3 install ansible==2.8.0
